bef4f2d6709792384d17ea0c6b6c901b1640b254
Harman <harmanjaggs@gmail.com>
2023-07-23 10:41:50 +0530

    total cost to hire kworkers

--- total-cost-to-hire-k-workers.cpp
+++ total-cost-to-hire-k-workers.cpp
@@ -1,33 +1,67 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
-using namespace std;
-
-long long totalCost(vector<int>& costs, int k, int candidates) {
-    priority_queue<int, vector<int>, greater<int>> q1, q2;
-    int i = 0, j = costs.size() - 1;
-    long long ans = 0;
-    while(k--) {
-        while(q1.size() < candidates && i <= j){
-            q1.push(costs[i++]);
+class Solution
+{
+public:
+    int shortestPathAllKeys(vector<string> &grid)
+    {
+        int x = -1, y = -1, m = grid.size(), n = grid[0].size(), max_len = -1;
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                char c = grid[i][j];
+                if (c == '@')
+                {
+                    x = i;
+                    y = j;
+                }
+                if (c >= 'a' && c <= 'f')
+                {
+                    max_len = max(c - 'a' + 1, max_len);
+                }
+            }
         }
-        while(q2.size() < candidates && i <= j){
-            q2.push(costs[j--]);
+        vector<int> start = {0, x, y};
+        queue<vector<int>> q;
+        unordered_set<string> visited;
+        visited.insert(to_string(0) + " " + to_string(x) + " " + to_string(y));
+        q.push(start);
+        int step = 0;
+        vector<vector<int>> dirs{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        while (!q.empty())
+        {
+            int size = q.size();
+            for (int k = 0; k < size; ++k)
+            {
+                vector<int> curr = q.front();
+                q.pop();
+                if (curr[0] == (1 << max_len) - 1)
+                    return step;
+                for (auto &d : dirs)
+                {
+                    int i = curr[1] + d[0];
+                    int j = curr[2] + d[1];
+                    int keys = curr[0];
+                    if (i >= 0 && i < m && j >= 0 && j < n)
+                    {
+                        char c = grid[i][j];
+                        if (c == '#')
+                            continue;
+                        if (c >= 'a' && c <= 'f')
+                            keys |= 1 << (c - 'a');
+                        if (c >= 'A' && c <= 'F' && ((keys >> (c - 'A')) & 1) == 0)
+                        {
+                            continue;
+                        }
+                        if (!visited.count(to_string(keys) + " " + to_string(i) + " " + to_string(j)))
+                        {
+                            visited.insert(to_string(keys) + " " + to_string(i) + " " + to_string(j));
+                            q.push({keys, i, j});
+                        }
+                    }
+                }
+            }
+            ++step;
         }
-        
-        int t1 = q1.size() ? q1.top() : INT_MAX;
-        int t2 = q2.size() ? q2.top() : INT_MAX;
-
-        auto& q = t1 <= t2 ? q1 : q2;
-        ans += q.top();
-        q.pop();
+        return -1;
     }
-    return ans;
-}
-
-int main() {
-    vector<int> t{17, 12, 10, 2, 7, 2, 11, 20, 8};
-    cout << totalCost(t, 3, 4);
-    return 0;
-}
+};
